e30735adbaa23301a5576088314dcd9dc22da6ab
k-nakamura <koya.nakamura@gmail.com>
2023-05-28 21:31:24 +0900

    index.tsxをセクションに分割 NewsSectionに引数を設定

---
+++ src/components/sections/Conference.tsx
@@ -0,0 +1,16 @@
+import useLocale from "@/components/hooks/locale";
+
+export default function ConferenceSection() {
+    const {t} = useLocale();
+
+    return (
+        <div>
+            <h1>
+                CONFERENCE
+            </h1>
+            <div>
+                TODO Conference
+            </div>
+        </div>
+    )
+}
---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,16 @@
+import useLocale from "@/components/hooks/locale";
+
+export default function HeroSection() {
+    const {t} = useLocale();
+
+    return (
+        <div>
+            <h1>
+                HERO
+            </h1>
+            <div>
+                TODO Hero
+            </div>
+        </div>
+    )
+}

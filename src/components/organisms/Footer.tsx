c45c9d2b3defd339234b16309a05a8d68e36a079
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-07-06 11:48:52 +0700

    feat: Prettierを追加

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,4 +1,4 @@
-import fsPromises from 'fs/promises';
+import fsPromises from "fs/promises";
 import Link from "next/link";
 import { Menu, Menus } from "@/types/menu";
 import Image from "next/image";
@@ -13,31 +13,28 @@ type SNS = {
   account: string;
   url: string;
   logo: string;
-}
+};
 
 const Footer = ({ menus = [] }: { menus: Menus }) => {
   const snsData = [
     {
-      name: 'twitter',
-      account: '@pyconjapan',
-      url: 'https://twitter.com/pyconjapan',
-      logo: 'twitter_w.svg',
+      name: "twitter",
+      account: "@pyconjapan",
+      url: "https://twitter.com/pyconjapan",
+      logo: "twitter_w.svg",
     },
     {
-      name: 'facebook',
-      account: '@PyConJP',
-      url: 'https://www.facebook.com/PyConJP/',
-      logo: 'facebook_w.svg',
+      name: "facebook",
+      account: "@PyConJP",
+      url: "https://www.facebook.com/PyConJP/",
+      logo: "facebook_w.svg",
     },
-  ]
-
+  ];
 
   return (
     <footer className="bg-gray-800 text-white text-center py-4 bottom-0">
       <div className="relative text-white font-noto">
-        <div
-          className="relative bg-center bg-no-repeat pt-7 pb-7 bg-tertiary-900 bg-footer lg:bg-right-bottom"
-        >
+        <div className="relative bg-center bg-no-repeat pt-7 pb-7 bg-tertiary-900 bg-footer lg:bg-right-bottom">
           <div className="w-11/12 mx-auto lg:w-10/12">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img
@@ -45,9 +42,7 @@ const Footer = ({ menus = [] }: { menus: Menus }) => {
               alt="2023-logo"
               className="mx-auto lg:ml-0 w-72"
             />
-            <div
-              className="flex flex-col w-1/2 gap-4 mx-auto footer-menu lg:flex-row lg:w-full lg:ml-0 lg:gap-0"
-            >
+            <div className="flex flex-col w-1/2 gap-4 mx-auto footer-menu lg:flex-row lg:w-full lg:ml-0 lg:gap-0">
               {menus.map((menu) => MenuContents(menu))}
             </div>
           </div>
@@ -100,12 +95,10 @@ const Footer = ({ menus = [] }: { menus: Menus }) => {
         </div>
       </div>
     </div>
-*/}
+            */}
           <div className="text-black bg-white">
             <div className="flex justify-center">
-              <div
-                className="flex flex-col pt-8 pb-3 lg:py-10 lg:flex-row lg:items-start"
-              >
+              <div className="flex flex-col pt-8 pb-3 lg:py-10 lg:flex-row lg:items-start">
                 <div className="flex-auto mb-2 lg:mb-0 lg:mr-2">
                   <a
                     href="https://www.pycon.jp"
@@ -121,7 +114,9 @@ const Footer = ({ menus = [] }: { menus: Menus }) => {
                   </a>
                 </div>
                 <div className="flex-row justify-center pl-8 pr-8 lg:ml-2 lg:p-0">
-                  <p className="lg:mt-2">主催：一般社団法人PyCon JP Association</p>
+                  <p className="lg:mt-2">
+                    主催：一般社団法人PyCon JP Association
+                  </p>
                   <p className="lg:mt-2">
                     PyCon JP 2022 is a production of the PyCon JP Association
                   </p>
@@ -131,55 +126,51 @@ const Footer = ({ menus = [] }: { menus: Menus }) => {
           </div>
         </div>
       </div>
-    </footer >
+    </footer>
   );
-}
+};
 
 const MenuContents = (menu: Menu) => {
   return (
     <div className="flex-1">
       <p>${menu.title}</p>
-      {menu.children.map((child) =>
+      {menu.children.map((child) => (
         <>
           <p>
-            <Link href={child.link}>
-              {child.title}
-            </Link>
+            <Link href={child.link}>{child.title}</Link>
           </p>
         </>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
 const SNSContents = (sns: SNS[]) => {
   return (
-    <div className="mr-2">
-      SNS
-    </div>
-      {sns.map((s) => <>
-      </>
-            <div key="sns.name">
-              <div class="inline-flex">
-                <div class="w-28">
-                  <outer-link class="text-secondary-300" :to="sns.url">
-                    {{ sns.account }}
-                  </outer-link>
-                </div>
-                <img
-                  class="inline w-6 h-6"
-                  :src="require(`@/assets/images/sns/${sns.logo}`)"
-                  :alt="sns.name"
-                />
-              </div>
-            </div >
-            )}
-
-  )
-}
+    <>
+      <div className="mr-2">SNS</div>
+      {sns.map((s) => (
+        <div key="s.name">
+          <div className="inline-flex">
+            <div className="w-28">
+              <Link className="text-secondary-300" href={s.url}>
+                TEST
+              </Link>
+            </div>
+            <img
+              className="inline w-6 h-6"
+              src="require(`@/assets/images/sns/${s.logo}`)"
+              alt="s.name"
+            />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async () => {
-  const filePath = path.join(process.cwd(), './src/data/menu.json');
+  const filePath = path.join(process.cwd(), "./src/data/menu.json");
   const jsonData = await fsPromises.readFile(filePath);
   const menus: Menus[] = JSON.parse(jsonData.toString());
 
@@ -189,6 +180,5 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   };
 };
-
 
 export default Footer;

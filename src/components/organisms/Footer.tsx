bcd80835c52bd8f7c2ff2129b3c15872cf48f859
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-07-10 20:59:29 +0700

    feat: add contact us and association logo

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
-import { MinusIcon } from "@heroicons/react/20/solid";
+import { ArrowRightIcon, MinusIcon } from "@heroicons/react/20/solid";
 import { Menu, menu } from "@/data/menu";
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
-import pyconApacLogo from "../../../public/footer-2023-logo.png";
 import { TFunction } from "i18next";
+
+import pyconApacLogo from "../../../public/footer-2023-logo.png";
+import pyconAssociationLogo from "../../../public/association-logo.svg";
 
 type SNS = {
   name: string;
@@ -32,9 +34,9 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="bg-[#111C3B]">
-      <div className="p-5">
-        <div className="p-5">
+    <footer>
+      <div className="p-5 bg-[#111C3B]">
+        <div className="p-5 flex flex-col gap-9">
           <div className="logo h-[95px]" style={{ position: "relative" }}>
             <Image
               src={pyconApacLogo}
@@ -46,8 +48,36 @@ const Footer = () => {
             />
           </div>
           <FooterMenu menus={menu} t={t} />
-          <hr className="h-px my-9 bg-primary-400 border-0 " />
+          <hr className="h-px bg-primary-400 border-0 " />
+          <ContactUs snsList={snsData} t={t} />
+          <Link
+            href="/"
+            className="flex w-full h-[32px] border-b-[1px] border-tertiary-100"
+          >
+            <p className="text-tertiary-100">
+              {t("PAST_EVENTS", { ns: "FOOTER" })}
+            </p>
+            <span>
+              <ArrowRightIcon className="h-6 text-tertiary-100 mx-auto" />
+            </span>
+          </Link>
         </div>
+      </div>
+      <div className="p-10 flex flex-col gap-10">
+        <div className="pyconjp-logo h-[85px]" style={{ position: "relative" }}>
+          <Image
+            src={pyconAssociationLogo}
+            alt="Pycon JP Association Logo"
+            fill
+            style={{
+              objectFit: "contain",
+            }}
+          />
+        </div>
+        <p>
+          主催: 一般社団法人PyCon JP Association PyCon JP 2022 is a production
+          of the PyCon JP Association
+        </p>
       </div>
     </footer>
   );
@@ -55,7 +85,7 @@ const Footer = () => {
 
 const FooterMenu = ({ menus, t }: { menus: Menu[]; t: TFunction }) => {
   return (
-    <nav className="pt-9">
+    <nav>
       <ul className="grid gap-5 w-max mx-auto">
         {menus.map((menu, menuKey) => (
           <li className="grid grid-cols-1 gap-3" key={menuKey}>
@@ -81,11 +111,19 @@ const FooterMenu = ({ menus, t }: { menus: Menu[]; t: TFunction }) => {
   );
 };
 
-const ContactUs = ({ t }: { t: TFunction }) => {
+const ContactUs = ({ snsList, t }: { snsList: SNS[]; t: TFunction }) => {
   return (
-    <div>
+    <div className="flex flex-col gap-3">
       <div>
-        <p>Follow us</p>
+        <p className="text-white text-sm">Follow Us!</p>
+      </div>
+      <div>
+        <p className="text-white text-sm">
+          {t("CONTACT_US", { ns: "FOOTER" })}
+        </p>
+      </div>
+      <div>
+        <p className="text-white text-sm">{t("READ_FAQ", { ns: "FOOTER" })}</p>
       </div>
     </div>
   );

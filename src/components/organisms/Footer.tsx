5fb383c80e03831aa1ed83acda1117e9d51531ab
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-07-10 16:12:23 +0700

    feat: add parent menus

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,11 +1,11 @@
-import fsPromises from 'fs/promises';
 import Link from "next/link";
-import { Menu, Menus } from "@/types/menu";
+import { Menu, menu } from "@/data/menu";
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
 import { GetStaticProps } from "next";
 import path from "path";
 import pyconApacLogo from "../../../public/footer-2023-logo.png";
+import { TFunction } from "i18next";
 // import { ExternalLink } from 'react-external-link';
 // import menus from "../../data/menu.json";
 
@@ -14,9 +14,9 @@ type SNS = {
   account: string;
   url: string;
   logo: string;
-}
+};
 
-const Footer = ({ menus = [] }: { menus: Menus }) => {
+const Footer = () => {
   const snsData = [
     {
       name: "twitter",
@@ -32,12 +32,13 @@ const Footer = ({ menus = [] }: { menus: Menus }) => {
     },
   ];
 
+  const { t } = useTranslation("PAGES");
 
   return (
     <footer className="bg-[#111C3B]">
       <div className="p-5">
         <div className="p-5">
-          <div className="h-[95px]" style={{ position: "relative" }}>
+          <div className="logo h-[95px]" style={{ position: "relative" }}>
             <Image
               src={pyconApacLogo}
               alt="Pycon APAC 2023 Logo"
@@ -47,66 +48,52 @@ const Footer = ({ menus = [] }: { menus: Menus }) => {
               }}
             />
           </div>
+          <div>{menu.map((v, k) => MenuContents(v, k, t))}</div>
         </div>
       </div>
     </footer>
   );
 };
 
-const MenuContents = (menu: Menu) => {
+const MenuContents = (menu: Menu, key: number, t: TFunction) => {
   return (
-    <div className="flex-1">
-      <p>${menu.title}</p>
-      {menu.children.map((child) =>
+    <div className="flex-1" key={key}>
+      <p>{t(menu.title)}</p>
+      {/*menu.children.map((child) => (
         <>
           <p>
-            <Link href={child.link}>
-              {child.title}
-            </Link>
+            <Link href={child.link}>{child.title}</Link>
           </p>
         </>
-      )}
+      ))*/}
     </div>
-  )
-}
-
-const SNSContents = (sns: SNS[]) => {
-  return (
-    <div className="mr-2">
-      SNS
-    </div>
-      {sns.map((s) => <>
-      </>
-            <div key="sns.name">
-              <div class="inline-flex">
-                <div class="w-28">
-                  <outer-link class="text-secondary-300" :to="sns.url">
-                    {{ sns.account }}
-                  </outer-link>
-                </div>
-                <img
-                  class="inline w-6 h-6"
-                  :src="require(`@/assets/images/sns/${sns.logo}`)"
-                  :alt="sns.name"
-                />
-              </div>
-            </div >
-            )}
-
-  )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const filePath = path.join(process.cwd(), './src/data/menu.json');
-  const jsonData = await fsPromises.readFile(filePath);
-  const menus: Menus[] = JSON.parse(jsonData.toString());
-
-  return {
-    props: {
-      menus,
-    },
-  };
+  );
 };
 
+// const SNSContents = (sns: SNS[]) => {
+//   return (
+//     <div className="mr-2">
+//       SNS
+//     </div>
+//       {sns.map((s) => <>
+//       </>
+//             <div key="sns.name">
+//               <div class="inline-flex">
+//                 <div class="w-28">
+//                   <outer-link class="text-secondary-300" :to="sns.url">
+//                     {{ sns.account }}
+//                   </outer-link>
+//                 </div>
+//                 <img
+//                   class="inline w-6 h-6"
+//                   :src="require(`@/assets/images/sns/${sns.logo}`)"
+//                   :alt="sns.name"
+//                 />
+//               </div>
+//             </div >
+//             )}
+//
+//   )
+// }
 
 export default Footer;

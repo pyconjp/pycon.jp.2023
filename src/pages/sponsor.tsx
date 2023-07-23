6a3c666e3b7d1419ee8d300ab512b9355cc06413
Akira Inamori <akira.inamori@tf-ebina.com>
2023-07-23 16:05:07 +0900

    Sponsor logoとダミーを配置

--- src/pages/sponsor.tsx
+++ src/pages/sponsor.tsx
@@ -32,13 +32,8 @@ const SponsorCard = ({ name, logo, url, description, width, height }: Omit<Spons
 
 const SilverSponsorCard = ({ name, logo, url, description, width, height }: Omit<Sponsor, "category">) => (
   <div className={"flex items-center gap-2 flex-col"}>
-    <div className={"flex-1"}>
-      <div>
-        <ExternalLink text={name} url={url} />
-      </div>
-    </div>
     <div>
-      {description}
+      <ExternalLink text={name} url={url} />
     </div>
   </div>
 );
@@ -47,7 +42,7 @@ const SponsorPage = ({ rows = [] }: { rows: Omit<Sponsor, "width" | "height">[] 
   const { t } = useTranslation("PAGES")
   const DiamondSection =
     <>
-      <SectionSubTitle title={'DIAMOND'} subTitle={'ダイヤモンド'} className='subTitle' />
+      <SectionSubTitle title={'DIAMOND'} subTitle={'ダイヤモンドスポンサー'} className='subTitle' />
       {
         rows.filter((row) => row.category === "diamond").map((row, index) => (
           <>
@@ -78,7 +73,7 @@ const SponsorPage = ({ rows = [] }: { rows: Omit<Sponsor, "width" | "height">[] 
 
   const PlatitnumSection =
     <>
-      <SectionSubTitle title={'PLATINUM'} subTitle={'プラチナ'} className='subTitle' />
+      <SectionSubTitle title={'PLATINUM'} subTitle={'プラチナスポンサー'} hasSeparator={true} className='subTitle' />
       <div className={"grid lg:grid-cols-3 grid-cols-2 gap-4 mx-44"}>
         {
           rows.filter((row) => row.category === "platinum").map((row, index) => (
@@ -98,7 +93,7 @@ const SponsorPage = ({ rows = [] }: { rows: Omit<Sponsor, "width" | "height">[] 
 
   const GoldSection =
     <>
-      <SectionSubTitle title={'GOLD'} subTitle={'ゴールド'} className='subTitle' />
+      <SectionSubTitle title={'GOLD'} subTitle={'ゴールドスポンサー'} hasSeparator={true} className='subTitle' />
       <div className={"grid lg:grid-cols-4 grid-cols-4 gap-4 mx-44"}>
         {
           rows.filter((row) => row.category === "gold").map((row, index) => (
@@ -118,7 +113,7 @@ const SponsorPage = ({ rows = [] }: { rows: Omit<Sponsor, "width" | "height">[] 
 
   const SilverSection =
     <>
-      <SectionSubTitle title={'SILVER'} subTitle={'シルバー'} className='subTitle' />
+      <SectionSubTitle title={'SILVER'} subTitle={'シルバースポンサー'} hasSeparator={true} className='subTitle' />
       <div className={"grid lg:grid-cols-4 grid-cols-4 gap-4 mx-44"}>
         {
           rows.filter((row) => row.category === "silver").map((row, index) => (
@@ -138,7 +133,7 @@ const SponsorPage = ({ rows = [] }: { rows: Omit<Sponsor, "width" | "height">[] 
 
   const BronzeSection =
     <>
-      <SectionSubTitle title={'BRONZE'} subTitle={'ブロンズ'} className='subTitle' />
+      <SectionSubTitle title={'BRONZE'} subTitle={'ブロンズスポンサー'} hasSeparator={true} className='subTitle' />
       <div className={"grid lg:grid-cols-4 grid-cols-4 gap-4 mx-44"}>
         {
           rows.filter((row) => row.category === "bronze").map((row, index) => (
@@ -158,7 +153,7 @@ const SponsorPage = ({ rows = [] }: { rows: Omit<Sponsor, "width" | "height">[] 
 
   const SpecialSection =
     <>
-      <SectionSubTitle title={'SPECIAL'} subTitle={'スペシャル'} className='subTitle' />
+      <SectionSubTitle title={'SPECIAL'} subTitle={'特別スポンサー'} hasSeparator={true} className='subTitle' />
       <div className={"grid lg:grid-cols-4 grid-cols-4 gap-4 mx-44"}>
         {
           rows.filter((row) => row.category === "special").map((row, index) => (
@@ -178,7 +173,7 @@ const SponsorPage = ({ rows = [] }: { rows: Omit<Sponsor, "width" | "height">[] 
 
   const PatronSection =
     <>
-      <SectionSubTitle title={'PATRON'} subTitle={'パトロン'} className='subTitle' />
+      <SectionSubTitle title={'PATRON'} subTitle={'パトロン'} hasSeparator={true} className='subTitle' />
       <div className={"grid lg:grid-cols-4 grid-cols-4 gap-4 mx-44"}>
         {
           rows.filter((row) => row.category === "patron").map((row, index) => (
@@ -200,13 +195,17 @@ const SponsorPage = ({ rows = [] }: { rows: Omit<Sponsor, "width" | "height">[] 
     <>
       <SectionTitle title='SPONSOR' subTitle='スポンサー' />
       <>
-        <div className={"my-16"}>
-          {DiamondSection}
+        <div className={"lg:mx-[80px] mx-[20px] mb-[60px] flex lg:flex-row flex-col"}>
+          <div className={"my-16"}>
+            {DiamondSection}
+          </div>
         </div>
       </>
       <>
-        <div className={"my-16"}>
-          {PlatitnumSection}
+        <div className={"lg:mx-[80px] mx-[20px] mb-[60px] flex lg:flex-row flex-col"}>
+          <div className={"my-16"}>
+            {PlatitnumSection}
+          </div>
         </div>
       </>
 

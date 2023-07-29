c94449ed5dd8ea2578e51fed53c947ca7ca097f1
Salahuddin Muhammad Iqbal <salahuddin.mi@gmail.com>
2023-07-29 17:03:32 +0700

    feat: add content container

--- src/pages/contents/beginners.tsx
+++ src/pages/contents/beginners.tsx
@@ -2,16 +2,40 @@ import Markdown from "@/components/elements/Markdown";
 import PageTitle from "@/components/elements/PageTitle";
 
 const Beginners = () => {
+  const content = `
+# はじめに
+
+Python初学者の方は、たくさんある書籍やWEBの情報で、まずどれから学んでいけばスムーズにPython学習が出来るかがわからないと思います。
+ここでは、Pythonをより効率的に学習してほしく紹介しています。
+
+## オススメPythonチュートリアル
+
+### Python Boot Camp テキスト
+
+https://pycamp.pycon.jp/textbook/
+
+このチュートリアルは、初学者向けに作られたテキストで、Pythonのインストールから基礎的な使い方まで掲載され、とてもわかりやすくまとめられています。
+
+Python Boot Campは、各地域に講師が赴き地域の運営スタッフが主体的にPythonを広める活動をしています。日本各地でのPythonを盛りあげ、各地のPythonistaとの連携強化、ゆくゆくは地域PyConの実施を目的とした活動です。
+
+ぜひ、実際のCampにも参加してみて下さい。
+    `;
   return (
     <>
       <PageTitle title="Contents for Beginners" />
       <div className="flex flex-col items-center self-stretch gap-8 px-4 py-5">
-        <div className="flex flex-col items-center self-stretch py-9 px-0">
-          <h1 className="text-3xl text-tertiary-900 italic tracking-[2.8px]">
-            Contents Name
-          </h1>
-          <h2>コンテツ名</h2>
-          <Markdown content={"## TITLE"} />
+        <div className="flex flex-col items-center self-stretch gap-8 py-9 px-0">
+          <div className="flex flex-col items-center gap-1">
+            <h1 className="text-4xl text-tertiary-900 italic tracking-[2.8px]">
+              Contents Name
+            </h1>
+            <h2 className="text-xl text-primary-600 leading-5 tracking-[0.14px] bg-white ">
+              コンテツ名
+            </h2>
+          </div>
+          <div className="flex flex-col gap-4 p-5 items-center shadow-lg">
+            <Markdown content={content} />
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,65 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        lg: "0px 4px 6px 0px rgba(0, 0, 0, 0.05), 0px 10px 15px 0px rgba(0, 0, 0, 0.10)",
+      },
+    },
     fontFamily: {
-      montserrat: ['Montserrat Alternates', 'sans-serif'],
+      montserrat: ["Montserrat Alternates", "sans-serif"],
     },
     colors: {
       primary: {
-        DEFAULT: '#90629D',
-        50: '#EDF1F7',
-        100: '#E9E0EB',
-        200: '#D3C0D8',
-        300: '#A682B0',
-        400: '#A682B0',
-        500: '#90629D',
-        600: '#734F7D',
-        700: '#3A273F',
-        800: '#563B5E',
-        900: '#1D141F',
+        DEFAULT: "#90629D",
+        50: "#EDF1F7",
+        100: "#E9E0EB",
+        200: "#D3C0D8",
+        300: "#A682B0",
+        400: "#A682B0",
+        500: "#90629D",
+        600: "#734F7D",
+        700: "#3A273F",
+        800: "#563B5E",
+        900: "#1D141F",
       },
       secondary: {
-        DEFAULT: '#4D70B2',
-        50: '#EDF1F7',
-        100: '#DBE2F0',
-        200: '#B8C6E0',
-        300: '#94A9D1',
-        400: '#708DC2',
-        500: '#4D70B2',
-        600: '#3D5A8F',
-        700: '#2E436B',
-        800: '#1F2D47',
-        900: '#0F1624',
+        DEFAULT: "#4D70B2",
+        50: "#EDF1F7",
+        100: "#DBE2F0",
+        200: "#B8C6E0",
+        300: "#94A9D1",
+        400: "#708DC2",
+        500: "#4D70B2",
+        600: "#3D5A8F",
+        700: "#2E436B",
+        800: "#1F2D47",
+        900: "#0F1624",
       },
       tertiary: {
-        DEFAULT: '#1ADFE5',
-        50: '#E8FCFC',
-        100: '#D1F9FA',
-        200: '#A3F2F5',
-        300: '#75ECF0',
-        400: '#47E5EB',
-        500: '#1ADFE5',
-        600: '#14B2B8',
-        700: '#0F868A',
-        800: '#0A595C',
-        900: '#052D2E',
+        DEFAULT: "#1ADFE5",
+        50: "#E8FCFC",
+        100: "#D1F9FA",
+        200: "#A3F2F5",
+        300: "#75ECF0",
+        400: "#47E5EB",
+        500: "#1ADFE5",
+        600: "#14B2B8",
+        700: "#0F868A",
+        800: "#0A595C",
+        900: "#052D2E",
       },
-      'white': {
-        DEFAULT: '#FFFFFF',
+      white: {
+        DEFAULT: "#FFFFFF",
       },
-      'alt-white': {
-        DEFAULT: '#FDFCEE',
+      "alt-white": {
+        DEFAULT: "#FDFCEE",
       },
-      'alt-black': {
-        DEFAULT: '#111C3B',
-      }
+      "alt-black": {
+        DEFAULT: "#111C3B",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
-
+  plugins: [require("@tailwindcss/typography")],
+};

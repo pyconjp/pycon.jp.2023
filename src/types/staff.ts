81ab712465762d58f0e108ee229cabd586f1cd63
Jonas Obrist <jonas.obrist@hennge.com>
2023-08-04 11:17:51 +0900

    Add support for mastodon for staff

--- src/types/staff.ts
+++ src/types/staff.ts
@@ -1,8 +1,12 @@
+export const DivisionNames = ["core", "chair", "reviewer", "day"] as const;
+export type Division = typeof DivisionNames[number];
+
 export type Staff = {
     name: string;
     icon: string;
     twitter: string;
     github: string;
     facebook: string;
-    division: string;
-  }
+    division: Division;
+    mastodon: string;
+}

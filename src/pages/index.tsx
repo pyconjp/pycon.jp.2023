ae66fdf67cfa210733ac418ccda1bf325a7b4ef6
k-nakamura <koya.nakamura@gmail.com>
2023-08-05 02:02:12 +0900

    favicon仮配置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home({ blogs = [] }: Props) {
         <title>PyCon APAC 2023</title>
         <meta name="description" content="PyCon APAC 2023" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <HeroSection />
       <NewsSection blogs={blogs} />

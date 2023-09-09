a77183116e512023071837c29e2054d219831648
k-nakamura <koya.nakamura@gmail.com>
2023-09-09 19:34:25 +0900

    画像が読み込めない問題を修正

--- src/components/elements/Modal.tsx
+++ src/components/elements/Modal.tsx
@@ -39,7 +39,7 @@ const Modal = ({session, onClose}: Props) => {
             <div className='whitespace-pre-line'>{session.description}</div>
             <hr className='my-6 border-secondary-300'/>
             <div className='flex gap-6'>
-              <Image src={speaker.avatar} alt={speaker.name} width={180} height={180}/>
+              <img src={speaker.avatar} alt={speaker.name} className='w-12'/>
               <div>
                 <div className='text-xl'>
                   {speaker.name}

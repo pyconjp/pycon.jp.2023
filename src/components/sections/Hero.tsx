9aaa4008c8e7df6a37c4ab348456c3abc02b3cf1
k-nakamura <koya.nakamura@gmail.com>
2023-09-25 21:52:16 +0900

    デザイン微修正

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -23,81 +23,84 @@ export default function HeroSection() {
             />
 
             <div className="flex flex-col items-center sm:px-[10%] my-[24px]">
-                <div className="">
-                    <div className="flex flex-row items-center mb-[16px]">
-                        <img src="/location.png" alt="" className="object-contain mx-[16px]" />
-                        <div className="flex sm:flex-row flex-col">
-                            <h2 suppressHydrationWarning className="text-tertiary-900 font-bold text-2xl">{t("VENUE")}</h2>
-                            <a
-                                href="https://www.toc.co.jp/saiji/ariake/access/"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                suppressHydrationWarning
-                                className="hover:opacity-70 text-lg text-secondary-600 font-bold underline underline-offset-8"
-                            >
-                                <div className="flex flex-row sm:mx-[16px]">
-                                    <p suppressHydrationWarning>{t("VENUE_INFO")}</p>
-                                    <Image
-                                        src={"/linkout_b.svg"}
-                                        alt={""}
-                                        width={20}
-                                        height={20}
-                                        className="object-contain ml-[5px]"
-                                    />
+                <div className='mx-auto'>
+                    <div>
+                        <div className="flex flex-row items-center mb-[16px]">
+                            <img src="/location.png" alt="" className="object-contain mx-[16px]" />
+                            <div className="flex sm:flex-row flex-col">
+                                <h2 suppressHydrationWarning className="text-tertiary-900 font-bold text-2xl">{t("VENUE")}</h2>
+                                <a
+                                  href="https://www.toc.co.jp/saiji/ariake/access/"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  suppressHydrationWarning
+                                  className="hover:opacity-70 text-lg text-secondary-600 font-bold underline underline-offset-8"
+                                >
+                                    <div className="flex flex-row sm:mx-[16px]">
+                                        <p suppressHydrationWarning>{t("VENUE_INFO")}</p>
+                                        <Image
+                                          src={"/linkout_b.svg"}
+                                          alt={""}
+                                          width={20}
+                                          height={20}
+                                          className="object-contain ml-[5px]"
+                                        />
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="flex flex-row items-center mb-[24px]">
+                            <img src="/calendar.png" alt="" className="mx-[16px]" />
+                            <div className="">
+                                <div className="flex sm:flex-row flex-col sm:items-center justify-between">
+                                    <h5 className="text-tertiary-600 font-montserrat italic mr-[16px]">Tutorial:</h5>
+                                    <p suppressHydrationWarning className="text-2xl text-left font-bold text-alt-black">{t("TUTORIAL_DATE")}</p>
                                 </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row items-center mb-[24px]">
-                        <img src="/calendar.png" alt="" className="mx-[16px]" />
-                        <div className="">
-                            <div className="flex sm:flex-row flex-col sm:items-center justify-between">
-                                <h5 className="text-tertiary-600 font-montserrat italic mr-[16px]">Tutorial:</h5>
-                                <p suppressHydrationWarning className="text-2xl text-left font-bold text-alt-black">{t("TUTORIAL_DATE")}</p>
-                            </div>
-                            <div className="flex sm:flex-row flex-col sm:items-center justify-between">
-                                <h5 className="text-tertiary-600 font-montserrat italic mr-[16px]">Conference:</h5>
-                                <p suppressHydrationWarning className="text-2xl font-bold text-alt-black">{t("CONFERENCE_DATE")}</p>
+                                <div className="flex sm:flex-row flex-col sm:items-center justify-between">
+                                    <h5 className="text-tertiary-600 font-montserrat italic mr-[16px]">Conference:</h5>
+                                    <p suppressHydrationWarning className="text-2xl font-bold text-alt-black">{t("CONFERENCE_DATE")}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="flex flex-row items-center mb-[16px]">
-                    <img src="/location.png" alt="" className="object-contain mx-[16px]" />
-                    <div className="flex sm:flex-row flex-col">
-                        <h2 suppressHydrationWarning className="text-tertiary-900 font-bold text-2xl">{t("VENUE_SPRINT")}</h2>
-                        <a
-                            href="https://hennge.com/jp/about/map/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            suppressHydrationWarning
-                            className="hover:opacity-70 text-lg text-secondary-600 font-bold underline underline-offset-8"
-                        >
-                            <div className="flex flex-row sm:mx-[16px]">
-                                <p suppressHydrationWarning>{t("VENUE_INFO")}</p>
-                                <Image
-                                    src={"/linkout_b.svg"}
-                                    alt={""}
-                                    width={20}
-                                    height={20}
-                                    className="object-contain ml-[5px]"
-                                />
+                    <div>
+                        <div className="flex flex-row items-center mb-[16px]">
+                            <img src="/location.png" alt="" className="object-contain mx-[16px]" />
+                            <div className="flex sm:flex-row flex-col">
+                                <h2 suppressHydrationWarning className="text-tertiary-900 font-bold text-2xl">{t("VENUE_SPRINT")}</h2>
+                                <a
+                                  href="https://hennge.com/jp/about/map/"
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  suppressHydrationWarning
+                                  className="hover:opacity-70 text-lg text-secondary-600 font-bold underline underline-offset-8"
+                                >
+                                    <div className="flex flex-row sm:mx-[16px]">
+                                        <p suppressHydrationWarning>{t("VENUE_INFO")}</p>
+                                        <Image
+                                          src={"/linkout_b.svg"}
+                                          alt={""}
+                                          width={20}
+                                          height={20}
+                                          className="object-contain ml-[5px]"
+                                        />
+                                    </div>
+                                </a>
                             </div>
-                        </a>
-                    </div>
-                </div>
-
-                <div className="flex flex-row items-center mb-[24px]">
-                    <img src="/calendar.png" alt="" className="mx-[16px]" />
-                    <div className="">
-                        <div className="flex sm:flex-row flex-col sm:items-center justify-between">
-                            <h5 className="text-tertiary-600 font-montserrat italic mr-[16px]">Sprint:</h5>
-                            <p suppressHydrationWarning className="text-2xl text-left font-bold text-alt-black">{t("SPRINT_DATE")}</p>
+                        </div>
+                        <div className="flex flex-row items-center mb-[24px]">
+                            <img src="/calendar.png" alt="" className="mx-[16px]" />
+                            <div className="">
+                                <div className="flex sm:flex-row flex-col sm:items-center justify-between">
+                                    <h5 className="text-tertiary-600 font-montserrat italic mr-[16px]">Sprint:</h5>
+                                    <p suppressHydrationWarning className="text-2xl text-left font-bold text-alt-black">{t("SPRINT_DATE")}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
                 <a href="https://pretix.eu/pyconjp/2023-apac/"
                     className="bg-alt-black hover:bg-secondary-700 rounded text-center text-alt-white w-full py-[14px]"
                 ><span>{t("TICKET_INFO")}</span></a>

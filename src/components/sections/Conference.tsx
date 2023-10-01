bee6c8f0b31beaa0d49c44403e7701dccef598bb
Akira Inamori <akira.inamori@tf-ebina.com>
2023-10-01 20:57:15 +0900

    基調講演者の順序を入れ替え

--- src/components/sections/Conference.tsx
+++ src/components/sections/Conference.tsx
@@ -14,18 +14,18 @@ export default function ConferenceSection() {
         <div className='my-20'>
           <SectionTitle title='Conference' subTitle='カンファレンス'/>
           <SectionSubTitle title={'Keynote'} subTitle={'基調講演'} className='subTitle'/>
+        <TextCard image={{ src: '/kita-photo-2020-07-07-trim.jpg', alt: t('SPEAKER_JP.IMG_ALT'), width: 300, height: 368 }} className='w-10/12 mx-auto'>
+          <Heading>
+            {t('SPEAKER_JP.NAME')}
+          </Heading>
+          <Markdown content={t('SPEAKER_JP.BIO')} components={{ Heading }} />
+        </TextCard>
+        <Separator />
           <TextCard image={{src: '/lorena_for_twitter.jpg', alt: t('SPEAKER_EN.IMG_ALT'), width: 300, height: 368}} className='w-10/12 mx-auto'>
             <Heading>
               {t('SPEAKER_EN.NAME')}
             </Heading>
             <Markdown content={t('SPEAKER_EN.BIO')} components={{Heading}}/>
-          </TextCard>
-          <Separator/>
-          <TextCard image={{src: '/kita-photo-2020-07-07-trim.jpg', alt: t('SPEAKER_JP.IMG_ALT'), width: 300, height: 368}} className='w-10/12 mx-auto'>
-            <Heading>
-              {t('SPEAKER_JP.NAME')}
-            </Heading>
-            <Markdown content={t('SPEAKER_JP.BIO')} components={{Heading}}/>
           </TextCard>
           <Separator/>
           {/* TODO ここにタイムテーブルへのリンク */}

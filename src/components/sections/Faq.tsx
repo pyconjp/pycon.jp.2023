0f8cbe566f9b0df26107f29d577e2dd338a9e5f6
k-nakamura <koya.nakamura@gmail.com>
2023-10-13 22:50:01 +0900

    mapのループ処理にindex追加、不要な枠を削除

--- src/components/sections/Faq.tsx
+++ src/components/sections/Faq.tsx
@@ -12,10 +12,10 @@ export default function FaqSection({ rows = [] }: { rows: Faq[] }) {
   return (
     <div className="flex-row items-center px-[5%] sm:px-[10%] xl:px-[20%] lg:py-[60px] py-20">
       <SectionTitle title="FAQ" subTitle="" />
-      {rows.map((row) => {
+      {rows.map((row, index) => {
         const translation = getTranslation(row, i18n.language);
         return (
-          <>
+          <div key={index}>
             <SectionSubTitle
               title={translation.question}
               subTitle=""
@@ -25,13 +25,9 @@ export default function FaqSection({ rows = [] }: { rows: Faq[] }) {
               {translation.answer}
             </p>
             <Separator />
-          </>
+          </div>
         );
       })}
-      <div className="lg:p-12 py-8 px-6 shadow rounded-lg">
-        <div className="mb-5"></div>
-        <ExternalLink text={t("ACCESS")} url="https://pyconjp.blogspot.com/" />
-      </div>
     </div>
   );
 }

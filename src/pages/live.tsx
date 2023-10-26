1465b7c31179acdf68dcf2974ed2725c8824187d
k-nakamura <koya.nakamura@gmail.com>
2023-10-26 16:43:32 +0900

    リダイレクト修正

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -1,9 +1,12 @@
 import {lives} from "@/data/lives";
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 const Live = () => {
   const router = useRouter();
-  router.push(`/live/${lives[0].options[0].value}`).then();
+  useEffect(() => {
+    router.push(`/live/${lives[0].options[0].value}`).then();
+  }, [router]);
 
   return (
     <>

edd3168de1ac452729154c5d2284f83c155c7d9e
Akira Inamori <akira.inamori@tf-ebina.com>
2023-10-26 16:39:20 +0900

    画像をキャッシュしないように細工を追加、モバイル向けでサイズが小さくなってしまう問題が残っている

--- src/pages/noc.tsx
+++ src/pages/noc.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from "react-i18next";
 
 export const ArchitectureComponent = () => {
   const { t } = useTranslation("CONTENTS");
+  const arc_img = `https://img.noc.pycon.jp/img/noc_topology.png?${(new Date).getTime()}`
   return (
     <>
       <SectionTitle title={'Architecture'} subTitle={t("NOC.ARCHITECTURE")} />
@@ -14,9 +15,9 @@ export const ArchitectureComponent = () => {
         <div className={"z-20 relative"}>
           <div className={"bg-[#ffffff] rounded-lg mt-[12px] mb-[12px] p-[200px] relative"}>
             <img
-              src="https://img.noc.pycon.jp/img/noc_topology.png"
+              src={arc_img}
               alt=""
-              className={"object-contain m-auto max-w-[100%] max-h-[100%] "}
+              className={"object-contain m-auto max-w-[70%] max-h-[80%] "}
             />
           </div>
         </div>
@@ -27,30 +28,33 @@ export const ArchitectureComponent = () => {
 
 export const RealtimeMonitorComponent = () => {
   const { t } = useTranslation("CONTENTS");
+  const monitor01_img = `https://img.noc.pycon.jp/img/noc_img1.png?${(new Date).getTime()}`
+  const monitor02_img = `https://img.noc.pycon.jp/img/noc_img2.png?${(new Date).getTime()}`
+  const monitor03_img = `https://img.noc.pycon.jp/img/noc_img3.png?${(new Date).getTime()}`
   return (
     <>
       <SectionTitle title={'Real-time Monitor'} subTitle={t("NOC.REALTIMEMONITOR")} />
 
       <div className={"relative"}>
         <div className={"z-20 relative"}>
-          <div className={"bg-[#ffffff] rounded-lg mt-[72px] mb-[72px] p-[200px] relative"}>
+          <div className={"bg-[#ffffff] rounded-lg mt-[144px] mb-[144px] p-[200px] relative"}>
             <div className="mt-10">
               <img
-                src="https://img.noc.pycon.jp/img/noc_img1.png"
+                src={monitor01_img}
+                alt=""
+                className={"object-contain mx-auto max-w-[100%] max-h-[100%]"}
+              />
+            </div>
+            <div className="mt-10">
+              <img
+                src={monitor02_img}
                 alt=""
                 className={"object-contain m-auto max-w-[100%] max-h-[100%] "}
               />
             </div>
             <div className="mt-10">
               <img
-                src="https://img.noc.pycon.jp/img/noc_img2.png"
-                alt=""
-                className={"object-contain m-auto max-w-[100%] max-h-[100%] "}
-              />
-            </div>
-            <div className="mt-10">
-              <img
-                src="https://img.noc.pycon.jp/img/noc_img3.png"
+                src={monitor03_img}
                 alt=""
                 className={"object-contain m-auto max-w-[100%] max-h-[100%] "}
               />

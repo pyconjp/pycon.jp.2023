0992531c8a1c0b1f067ddaf8fd79e30101bd3f8e
k-nakamura <koya.nakamura@gmail.com>
2025-01-06 21:34:35 +0900

    Removing image optimization.

---
+++ 2023/_next/static/Qkd-6GepghSfmSqvgiOlA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Flightning-talk","\u002Flive\u002F[id]","\u002Fposter","\u002Fsponsor","\u002Fsprint","\u002Fstaff","\u002Ftimetable","\u002Ftutorial","\u002Ftutorial\u002F[code]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
